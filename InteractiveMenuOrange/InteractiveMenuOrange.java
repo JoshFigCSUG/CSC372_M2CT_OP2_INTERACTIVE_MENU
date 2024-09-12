6d688b06ba6855dbcd3c0c5397af7f5b239e924e
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-09-12 13:49:35 -0600

    Update InteractiveMenuOrange.java
    add menu items

--- InteractiveMenuOrange/InteractiveMenuOrange.java
+++ InteractiveMenuOrange/InteractiveMenuOrange.java
@@ -9,6 +9,13 @@ public class InteractiveMenuOrange extends JFrame {
         // Create Menu Bar and Menu
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Options");
+
+        // Create MenuItems 1 through 4
+        JMenuItem printDateTimeMenuItem = new JMenuItem("Print Date and Time");
+        JMenuItem saveToLogMenuItem = new JMenuItem("Save to log.txt");
+        JMenuItem colorChangeMenuItem = new JMenuItem("Change Background Color. Current Hue: 0");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        
         setJMenuBar(menuBar);
 
     }
