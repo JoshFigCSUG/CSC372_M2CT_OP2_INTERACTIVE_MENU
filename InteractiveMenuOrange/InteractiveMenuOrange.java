0c70f12d3bae0c2c01828007579ffa4a0ec9f55f
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-09-12 13:46:13 -0600

    Update InteractiveMenuOrange.java
    create menu bar

--- InteractiveMenuOrange/InteractiveMenuOrange.java
+++ InteractiveMenuOrange/InteractiveMenuOrange.java
@@ -6,6 +6,10 @@ public class InteractiveMenuOrange extends JFrame {
         setSize(500, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Create Menu Bar and Menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Options");
+
     }
 
     public static void main(String[] args) {
